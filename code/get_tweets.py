4fc6b9fa817f08db45729f9f538d52e2f9a5f9f6
Mihir Bindal <59363084+MihirBindal@users.noreply.github.com>
2020-05-18 20:11:56 +0530

    Added code for data cleaning
    A script to clean the data has been added. Also, separate files are created to make the repo look cleaner.

--- code/get_tweets.py
+++ code/get_tweets.py
@@ -2,7 +2,6 @@ from tweepy import Cursor
 from twitter_credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 import tweepy
 import csv
-import _json
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -10,16 +9,17 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
 def insert_to_csv(hashtag_phrase):
-    with open("tweet_output.csv", "w", encoding="utf-8") as file:
+    with open("..\output file\/tweet_output.csv", "w", encoding="utf-8") as file:
         w = csv.writer(file)
-        w.writerow(["timestamp", "tweet_text", "username", "all_hashtags", "followers_count"])
+        w.writerow(["timestamp", "tweet_text", "username", "all_hashtags", "followers_count", "favorite_count", "retweets_count", "length_of_tweet"])
         for tweet in Cursor(api.search, q=hashtag_phrase + "-filter:retweets", lang="en", tweet_mode="extended").items(
-                100):
+                300):
             w.writerow([tweet.created_at, tweet.full_text.replace("\n", "").encode("utf-8"),
                         tweet.user.screen_name.encode("utf-8"),
-                        [e["text"] for e in tweet._json["entities"]["hashtags"]], tweet.user.followers_count])
+                        [e["text"] for e in tweet._json["entities"]["hashtags"]], tweet.user.followers_count, tweet.favorite_count, tweet.retweet_count, len(tweet.full_text.replace("\n", "").encode("utf-8"))])
 
 
 if __name__ == '__main__':
-    insert_to_csv("#coronavirus AND #corona")
-# posts = api.user_timeline(screen_name="BillGates", count=25, lang="en", tweet_mode="extended")
+    phrase= input("Enter a phrase to find output: ")
+    insert_to_csv(hashtag_phrase=phrase)
+
